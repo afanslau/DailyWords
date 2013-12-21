ed9669385120848cfc4b3e3f6446a81b7f393846
Adam Fanslau <afanslau@gmail.com>
2013-12-21 15:17:21 -0500

    Initial Commit

---
+++ DailyWords/AFAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AFAppDelegate.h
+//  DailyWords
+//
+//  Created by Adam D. Fanslau on 12/21/13.
+//  Copyright (c) 2013 Adam D. Fanslau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
